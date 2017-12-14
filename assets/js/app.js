0c2c4108f923e55a4d55cab8b3d1d0aaaaa06f47
pinalef <tierralavista@gmail.com>
2017-12-14 20:47:24 -0300

    actualizando diseño

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,11 +3,10 @@ $('#splashscreen').hide();
 $('#home').hide();
 $('body').css({'background-color': 'linear-gradient(to left bottom, #76278F, #2B1E66)', 'background-size': 'cover'});
 $('#splashscreen').show();
-$('#splashscreen').fadeOut(3000).hide(1000);
+$('#splashscreen').fadeOut(2000).hide(1000);
 setTimeout(function(){
-    $('#home').delay(3000).fadeIn(1000);
+    $('#home').delay(500).fadeIn(500);
     $('body').css({'background-color': 'transparent'});
-    $('#home').css({'background-image' : 'url(https://media.giphy.com/media/a2QExYEHYCQJq/giphy_s.gif)', 'background-size':'cover'});
 }, 3000)
 
 $('#sign-up-btn').click(function(){
